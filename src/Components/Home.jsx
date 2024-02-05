ce87215465e211bdd09cbd9c7345116120325be3
Melissa2002 <mosherof@ucsd.edu>
2024-02-05 02:54:33 -0800

    h

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,13 +4,13 @@ import Me from "./Attach0.jpg"
 function Home() {
     return (
         <div class="row">
-            <div class="column left" width="60%">
+            <div class="column left">
             <p> Hi there! My name is Melissa, and this is my personal portfolio where I hope
                 to document coding projects. Please enjoy learning about me, and don't be
                 shy to reach out if you have any comments or questions. 
             </p>
             </div>
-            <div class="column right" width="40%">
+            <div class="column right">
                 <img src={Me}  alt="MyProfile" width="80%" height="auto"/>
             </div>
         </div>
