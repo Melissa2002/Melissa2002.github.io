f55688e13146dc43da6cd33c300385f24833eeef
Melissa2002 <mosherof@ucsd.edu>
2024-02-05 01:37:47 -0800

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,9 +6,9 @@ function Contact() {
         <>
         <h2>Contact</h2>
          <p> Let's chat! I am always looking for new projects and collaborations. </p>
-        <a  href="mailto:melissa.e.osheroff@gmail.com?subject=Business%20Inquiries%20Through%20Portfolio" style={{ color: "#61dafb", padding: "10px 20px" }}
+        <a  href="mailto:melissa.e.osheroff@gmail.com?subject=Business%20Inquiries%20Through%20Portfolio"  target="_top" 
         >Email: Melissa.e.osheroff@gmail.com</a>
-        <Button  href="mailto:melissa.e.osheroff@gmail.com?subject=Business%20Inquiries%20Through%20Portfolio" style={{ color: "#61dafb", padding: "10px 20px" }}
+        <Button  href="tel:4155901894" 
         >Phone: (415) 590-1894</Button>
         </>
     
