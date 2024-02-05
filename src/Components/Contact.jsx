b4fab9094baeb7c7545ca46031a7fb8c6dc7e51b
Melissa2002 <mosherof@ucsd.edu>
2024-02-05 00:42:47 -0800

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,8 +6,8 @@ function Contact() {
         <>
         <h2>Contact</h2>
          <p> Let's chat! I am always looking for new projects and collaborations. </p>
-        <Button  href="https://example.com" style={{ color: "#61dafb", padding: "10px 20px" }}
-        >Email: Melissa.e.osheroff@gmail.com</Button>
+        <a  href="mailto:melissa.e.osheroff@gmail.com?subject=Business%20Inquiries%20Through%20Portfolio" style={{ color: "#61dafb", padding: "10px 20px" }}
+        >Email: Melissa.e.osheroff@gmail.com</a>
         <Button  href="mailto:melissa.e.osheroff@gmail.com?subject=Business%20Inquiries%20Through%20Portfolio" style={{ color: "#61dafb", padding: "10px 20px" }}
         >Phone: (415) 590-1894</Button>
         </>
