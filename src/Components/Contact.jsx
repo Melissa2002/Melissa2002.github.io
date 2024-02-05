2945190d74711b7810d7645e88a7635dc2fe4797
Melissa2002 <mosherof@ucsd.edu>
2024-02-05 15:06:38 -0800

    x

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import Email from './Email.jpeg'
 
 function Contact() {
     return (
         <>
         <h2>Contact</h2>
          <p> Let's chat! I am always looking for new projects and collaborations. </p>
+         <button href="mailto:melissa.e.osheroff@gmail.com?subject=Business%20Inquiries%20Through%20Portfolio"  target="_top">
+            <img src={Email} border="0" />
+        </button>
         <a  href="mailto:melissa.e.osheroff@gmail.com?subject=Business%20Inquiries%20Through%20Portfolio"  target="_top" 
         >Email: Melissa.e.osheroff@gmail.com</a>
         <br/>
