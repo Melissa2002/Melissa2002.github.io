f7ccfd9306b6a9724d69661406cbe87bab7aa011
Melissa2002 <mosherof@ucsd.edu>
2024-02-05 02:02:53 -0800

    add resume

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -8,6 +8,7 @@ function Contact() {
          <p> Let's chat! I am always looking for new projects and collaborations. </p>
         <a  href="mailto:melissa.e.osheroff@gmail.com?subject=Business%20Inquiries%20Through%20Portfolio"  target="_top" 
         >Email: Melissa.e.osheroff@gmail.com</a>
+        <br/>
         <Button  href="tel:4155901894" 
         >Phone: (415) 590-1894</Button>
         </>
