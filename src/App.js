592b8b9951f1c1545d432746833e7f49e32f4ec2
Melissa2002 <mosherof@ucsd.edu>
2024-05-13 21:21:07 -0700

    changing to an html index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
-import './App.css';
-import React from "react";
-import Home from "./Components/Home.jsx"
-import Header from "./Components/Name.jsx"
-import Contact from "./Components/Contact.jsx"
 import { useRef } from "react"
-import AboutMe from "./Components/AboutMe.jsx"
-import Projects from "./Components/Projects.jsx"
 
 
 function App() {
@@ -20,32 +13,4 @@ function App() {
       behavior: "smooth"
     })
   }
-    
-
-  return (
-    <>
-      
-    <Header></Header>
-    
-      <button onClick={() => scrollToSection(home)} className="link"> Home </button>
-      <button onClick={() => scrollToSection(projects)} className="link"> Projects </button>
-      <button onClick={() => scrollToSection(contact)} className="link"> Contact </button>
- 
-    <div ref={home} className="home">
-      <Home></Home>
-    </div>
-    <div ref={projects} className="projects">
-      <Projects></Projects>
-    </div>
-    <div ref={contact} className="contact">
-      <Contact></Contact>
-    </div>
-
-
-   
-        
-    </>
-  );
 }
-
-export default App;
