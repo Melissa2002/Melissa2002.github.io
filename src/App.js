4cfb69c132e2a5f9f48997a85d085e2c3c68871e
Melissa2002 <mosherof@ucsd.edu>
2025-03-25 22:24:14 -0700

    Adding project main frame

--- src/App.js
+++ src/App.js
@@ -12,48 +12,26 @@ function init() {
   projectButton.addEventListener("click", () => scrollHome("projects"));
 
   const resumeButton = document.getElementById("resume-button");
-  resumeButton.addEventListener("click", ()=> openLink(resumeButton.getAttribute("href")));
-
-  const projectMenu = document.getElementById("projectmenu");
-  projectButton.addEventListener("mouseover", () => showMenu(projectMenu));
-  projectButton.addEventListener("mouseleave", () => leaveMenu(projectMenu));
-
-  //const project2 = document.getElementById("project-2");
-  //project2.addEventListener("click", () => scrollHome("project2"))
-
-  //const project3 = document.getElementById("project-3");
-  //project3.addEventListener("click", () => scrollHome("project3"))
-  const email = document.getElementById("contact-email");
-  email.addEventListener("click", ()=> openLink(email.getAttribute("href")));
+  resumeButton.addEventListener("click", ()=> scrollHome("resume"));
 
   const cell = document.getElementById("contact-cell");
   cell.addEventListener("click", ()=> openLink(cell.getAttribute("href")));
 
-  const project1 = document.getElementById("project1");
-  project1.addEventListener("click", ()=> openLink(project1.getAttribute("href")));
-
-  const project2 = document.getElementById("project2");
-  project2.addEventListener("click", ()=> openLink(project2.getAttribute("href")));
-
-  const project3 = document.getElementById("project3");
-  project3.addEventListener("click", ()=> openLink(project3.getAttribute("href")));
-
-  const project4 = document.getElementById("project4");
-  project4.addEventListener("click", ()=> openLink(project4.getAttribute("href")));
-
   const contactButton = document.getElementById("contact-button");
   contactButton.addEventListener("click", () => scrollHome("contact"));
 
+  const funButton = document.getElementById("fun-button");
+  funButton.addEventListener("click", () => scrollHome("fun"));
 
 
 }
 
-function leaveMenu(menu) {
-  const children = menu.children;
-  for (let i = 0; i < children.length; i++) {
-    children[i].style.display = "none";
-  }
-}
+// function leaveMenu(menu) {
+//   const children = menu.children;
+//   for (let i = 0; i < children.length; i++) {
+//     children[i].style.display = "none";
+//   }
+// }
 
 function openLink(href) {
   window.open(href, "_blank");
@@ -70,4 +48,15 @@ function showMenu(menu) {
   for (let i = 0; i < children.length; i++) {
     children[i].style.display = "block";
   }
+}
+
+function filterProjects(category) {
+  const allProjects = document.querySelectorAll('.project-card');
+  allProjects.forEach(project => {
+      if (category === 'all' || project.classList.contains(category)) {
+          project.style.display = 'block';
+      } else {
+          project.style.display = 'none';
+      }
+  });
 }
